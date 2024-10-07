00d20cc821cbd76732e80bfc3c77211cf047bda8
Stefano Giordano <Stef@Campanario.local>
2024-10-06 23:08:03 -0300

    feat: connection handler

--- app/src/app.js
+++ app/src/app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoose = require('mongoose')
+
+try {
+  mongoose.connect(process.env.MONGO_URL)
+  console.log("[DB:OK] CONNECT SUCCESSFULLY AT", process.env.MONGO_URL)
+} catch(error){
+  console.log("[DB:ERROR]", error)
+}
 
 app.listen(process.env.PORT, () => {
-  console.log(`App listening on port ${process.env.PORT}`)
+  console.log(`[APP:OK] App listening on port ${process.env.PORT}`)
 })
 
